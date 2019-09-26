292119359202d6225ec8932c72b23d1daaafefc1
CBreakr <rob.notwicz@gmail.com>
2019-09-26 17:55:47 -0400

    set up redux

--- src/index.js
+++ src/index.js
@@ -3,4 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+import rootReducer from "./Reducers/RootReducer";
+
+const store = createStore(rootReducer,
+  {},
+window.__REDUX_DEVTOOLS_EXTENSION__ &&
+window.__REDUX_DEVTOOLS_EXTENSION__());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App store={store} />
+  </Provider>,
+  document.getElementById('root'));

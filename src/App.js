e8fc2b18899c52a97d0bc1b412f3c1c177d8f880
CBreakr <rob.notwicz@gmail.com>
2019-09-26 18:25:10 -0400

    route structure for pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css';
 
+import { Route, Switch } from "react-router-dom";
+
+import CharacterPage from "./Containers/CharacterPageContainer";
+import FilmPage from "./Containers/FilmPageContainer";
+
 class App extends React.Component {
   render(){
     return (
       <div className="App">
-
+        <h1>Star Wars Header for now</h1>
+        <Switch>
+          <Route exact path="/" component={CharacterPage} />
+          <Route exact path="/films" component={FilmPage} />
+        </Switch>
       </div>
     );
   }

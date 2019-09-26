69b2142e37224d6213bdd0a89ce932a20f11805a
CBreakr <rob.notwicz@gmail.com>
2019-09-26 17:55:16 -0400

    removed test code

--- src/App.js
+++ src/App.js
@@ -2,21 +2,6 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
-
-  async componentDidMount(){
-    // fetch the configuration
-    const response = await fetch("../config/characters.json",
-      {
-         headers : {
-           'Content-Type': 'application/json',
-           'Accept': 'application/json'
-         }
-     });
-    const data = await response.json();
-
-    console.log("characters", data);
-  }
-
   render(){
     return (
       <div className="App">

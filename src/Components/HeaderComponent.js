69cbe2200009f5257b0fcdaaedbc969264d0f82a
CBreakr <rob.notwicz@gmail.com>
2019-09-30 16:02:50 -0400

    added character name to the header display, so be used for mobile layout instead of card

--- src/Components/HeaderComponent.js
+++ src/Components/HeaderComponent.js
@@ -4,6 +4,9 @@ import { Link } from "react-router-dom";
 
 class Header extends React.Component {
   render() {
+
+    console.log("header state", this.props);
+
     let additionalClass = "";
     if(this.props.isFilmPage){
       additionalClass = "filmHeader";
@@ -13,20 +16,33 @@ class Header extends React.Component {
     }
 
     const classes = `header ${additionalClass}`;
+    let character = null;
+    if(this.props.characterId){
+      character = this.props.characters.find(char => {
+        // matching string to number here
+        return this.props.characterId == char.id;
+      });
+    }
 
     return (
       <div className={classes}>
         <h1>STAR<br />WARS</h1>
-        <h2 className="backgroundBlur">DISCOVER THE FILMS OF YOUR FAVORITE CHARACTERS</h2>
         {this.props.isFilmPage
-          ? <div className="backLinkContainer">
-            <Link className="backLink" to="/">
-              <span>
-                Pick Another Character
-              </span>
-            </Link>
+          ? <>
+            <div className="characterHeaderName backgroundBlur">
+              {character.name}
+            </div>
+            <div className="backLinkContainer">
+              <Link className="backLink" to="/">
+                <span>
+                  Pick Another Character
+                </span>
+              </Link>
+            </div>
+          </>
+          :<div className="instructions backgroundBlur">
+            Select a character to explore their films
           </div>
-          :""
         }
       </div>
     );

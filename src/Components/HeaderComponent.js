73fb2a796c7bf481389ac1771750d6e4d14514d4
CBreakr <rob.notwicz@gmail.com>
2019-09-30 00:30:00 -0400

    set header height based on route

--- src/Components/HeaderComponent.js
+++ src/Components/HeaderComponent.js
@@ -4,8 +4,18 @@ import { Link } from "react-router-dom";
 
 class Header extends React.Component {
   render() {
+    let additionalClass = "";
+    if(this.props.isFilmPage){
+      additionalClass = "filmHeader";
+    }
+    else {
+      additionalClass = "characterHeader";
+    }
+
+    const classes = `header ${additionalClass}`;
+
     return (
-      <div className="header">
+      <div className={classes}>
         <h1>STAR<br />WARS</h1>
         <h2 className="backgroundBlur">DISCOVER THE FILMS OF YOUR FAVORITE CHARACTERS</h2>
         {this.props.isFilmPage

4ab8a5bd7c03555bb321c1caad9ba6a74ab55220
CBreakr <rob.notwicz@gmail.com>
2019-10-07 20:09:46 -0400

    Updates

---
+++ precache-manifest.801691a6b085b751c60dc2ac95c00631.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "401e64b2fcbd963d427fa2cf4de7c50a",
+    "url": "/ATTCK_StarWars/index.html"
+  },
+  {
+    "revision": "ec68448af717534fb28c",
+    "url": "/ATTCK_StarWars/static/css/main.d95c6adc.chunk.css"
+  },
+  {
+    "revision": "c42156962d602f898902",
+    "url": "/ATTCK_StarWars/static/js/2.3250a279.chunk.js"
+  },
+  {
+    "revision": "ec68448af717534fb28c",
+    "url": "/ATTCK_StarWars/static/js/main.39427a13.chunk.js"
+  },
+  {
+    "revision": "8784ecda6f0ef90effb7",
+    "url": "/ATTCK_StarWars/static/js/runtime-main.e0cc40a5.js"
+  }
+]);

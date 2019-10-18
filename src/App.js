6571e9cb01e7bcaeae1dbde11f8f0895ea5422bc
CBreakr <rob.notwicz@gmail.com>
2019-10-18 03:38:57 -0400

    remove path changes and B's header

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ class App extends React.Component {
   render(){
     return (
       <div className="App">
-        <h1>BBBBBBBBBBBBBBBBBBB</h1>
         <Header />
         <div className="mainContent">
           <Switch>
